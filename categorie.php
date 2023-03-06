9fbf24476893c82550af24d1d1b66f38e6cb765d
MonicaAuditore <mauditore@live.it>
2023-03-06 14:44:41 +0100

    add_prodotti

---
+++ categorie.php
@@ -0,0 +1,13 @@
+<?php
+
+class Categorie {
+  public $cani;
+  public $gatti;
+
+    function __construct($cani, $gatti) {
+    $this->cani;
+    $this->gatti;
+    }
+}
+
+?>
---
+++ prodotti.php
@@ -0,0 +1,51 @@
+<?php
+
+class Prodotto {
+  public $nome;
+  public $descrizione;
+  public $prezzo;
+  public $immagine;
+
+  public function __construct($nome, $descrizione, $prezzo, $immagine) {
+      $this->nome = $nome;
+      $this->descrizione = $descrizione;
+      $this->prezzo = $prezzo;
+      $this->immagine = $immagine;
+  }
+}
+
+class ProdottoCibo extends Prodotto {
+  public $seccoUmido;
+  public $dimensioneConfezione;
+
+  public function __construct($nome, $descrizione, $prezzo, $immagine, $seccoUmido, $dimensioneConfezione) {
+      parent::__construct($nome, $descrizione, $prezzo, $immagine);
+      $this->seccoUmido = $seccoUmido;
+      $this->dimensioneConfezione = $dimensioneConfezione;
+  }
+}
+
+class ProdottoGioco extends Prodotto {
+  public $materiale;
+  public $funzione;
+
+  public function __construct($nome, $descrizione, $prezzo, $immagine, $materiale, $funzione) {
+      parent::__construct($nome, $descrizione, $prezzo, $immagine);
+      $this->materiale = $materiale;
+      $this->funzione = $funzione;
+  }
+}
+
+class ProdottoCuccia extends Prodotto {
+  public $dimensione;
+  public $materiale;
+
+  public function __construct($nome, $descrizione, $prezzo, $immagine, $dimensione, $materiale) {
+      parent::__construct($nome, $descrizione, $prezzo, $immagine);
+      $this->dimensione = $dimensione;
+      $this->materiale = $materiale;
+  }
+}
+
+
+?>
