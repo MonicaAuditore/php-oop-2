580ce6ecc6249cb081a876893b4a8a4c3226e64a
MonicaAuditore <mauditore@live.it>
2023-03-06 17:54:01 +0100

    add_prodotto Gatto

--- singoliProdotti.php
+++ singoliProdotti.php
@@ -6,9 +6,14 @@ $categoriaUno = new CategoriaCane("medio", "adulto", "img/cane.png", "qualsiasi 
 $cardUno = new Card($prodottoUno, $categoriaUno);
 echo $cardUno->printCard();
 
-$prodottoUno = new ProdottoGioco("Puzzle", "giocattoli addestramento interattivo", 19.99 . ' Euro', "img/gioco-cane.png", "secco", "2 kg");
-$categoriaUno = new CategoriaCane("qualsiasi", "adulto", "img/cane.png", "qualsiasi razza");
-$cardUno = new Card($prodottoUno, $categoriaUno);
-echo $cardUno->printCard();
+$prodottoDue = new ProdottoGioco("Puzzle per cani", "giocattoli addestramento interattivo", 19.99 . ' Euro', "img/gioco-cane.png", "legno", "sviluppo intelligenza");
+$categoriaDue = new CategoriaCane("qualsiasi", "adulto", "img/cane.png", "qualsiasi razza");
+$cardDue = new Card($prodottoDue, $categoriaDue);
+echo $cardDue->printCard();
+
+$prodottoTre = new ProdottoCuccia("Cuscino per gatto", "cuscino morbido per gatti", 17.95 . ' Euro', "img/cuccia-gatto.png", "18L x 40l x 40Sp cm", "Tessuto");
+$categoriaTre = new CategoriaGatto("qualsiasi", "qualsiasi", "img/gatto.png", "qualsiasi lunghezza");
+$cardTre = new Card($prodottoTre, $categoriaTre);
+echo $cardTre->printCard();
 
 ?>
