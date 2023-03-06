1ba6a9722fbfcc385904d5f556673f641aaec0ab
MonicaAuditore <mauditore@live.it>
2023-03-06 14:58:45 +0100

    add_categorie

--- categorie.php
+++ categorie.php
@@ -1,13 +1,30 @@
 <?php
 
-class Categorie {
-  public $cani;
-  public $gatti;
+class Categoria {
+  public $taglia;
+  public $eta;
 
-    function __construct($cani, $gatti) {
-    $this->cani;
-    $this->gatti;
+    function __construct($taglia, $eta) {
+    $this->taglia = $taglia;
+    $this->eta = $eta;
     }
+}
+
+class CategoriaCane extends Categoria {
+  public $razza;
+
+  function __construct($taglia, $eta, $razza) {
+    parent::__construct($taglia, $eta);
+    $this->razza = $razza;
+  }
+}
+class CategoriaGatto extends Categoria {
+  public $pelo;
+  
+  function __construct($taglia, $eta, $pelo) {
+    parent::__construct($taglia, $eta);
+    $this->pelo = $pelo; 
+  }
 }
 
 ?>
