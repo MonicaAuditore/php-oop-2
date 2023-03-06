56c7665891fb961c83cbcc30687f736e60d005b8
MonicaAuditore <mauditore@live.it>
2023-03-06 17:44:23 +0100

    add_prodotti in cardProdotti

--- cardProdotti.php
+++ cardProdotti.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . './categorie.php';
 require_once __DIR__ . './prodotti.php';
 
+
 class Card {
   public $prodotto;
   public $categoria;
@@ -13,12 +14,16 @@ class Card {
   
   public function printCard() {
   //card del prodotto
-  echo '<div class="card">';
-  echo '<img src="' . $this->prodotto->immagine . '" alt="' . $this->prodotto->nome . '">';
+  echo '<div style="display: flex; gap: 60px; justify-content: flex-start;
+  align-items: flex-end;">';
+  echo '<div class="card" style="text-align: center;
+  padding: 28px;
+  border-radius: 30px;">';
+  echo '<img class="prodotto-img" style="width: 190px;" src="' . $this->prodotto->immagine . '" alt="' . $this->prodotto->nome . '">';
 
   echo '<h2>' . $this->prodotto->nome . '</h2>';
   echo '<h3>' . $this->prodotto->prezzo . '</h3>';
-  echo '<img src="' . $this->categoria->icona . '">';
+  echo '<img class="categoria-icona" style="width: 42px;" src="' . $this->categoria->icona . '">';
 
   //instanceof verifica se un oggetto è un'istanza di una classe padre
   if ($this->prodotto instanceof ProdottoCibo) {
@@ -32,10 +37,10 @@ class Card {
   }
   echo '</div>';
   }
-    
+  
   
 }
-
+echo '</div>';
 
 
 
--- singoliProdotti.php
+++ singoliProdotti.php
@@ -1,8 +1,13 @@
 <?php 
 require_once __DIR__ . '/cardProdotti.php';
 
-$prodottoUno = new ProdottoCibo("Crocchette per cani Yarrah", "Cibo secco per cani adulti", 14.95 . ' Euro', "img/crocchette-cane.jpg", "secco", "2 kg");
+$prodottoUno = new ProdottoCibo("Crocchette per cani Yarrah", "Cibo secco per cani adulti", 14.95 . ' Euro', "img/crocchette-cane.png", "secco", "2 kg");
 $categoriaUno = new CategoriaCane("medio", "adulto", "img/cane.png", "qualsiasi razza");
+$cardUno = new Card($prodottoUno, $categoriaUno);
+echo $cardUno->printCard();
+
+$prodottoUno = new ProdottoGioco("Puzzle", "giocattoli addestramento interattivo", 19.99 . ' Euro', "img/gioco-cane.png", "secco", "2 kg");
+$categoriaUno = new CategoriaCane("qualsiasi", "adulto", "img/cane.png", "qualsiasi razza");
 $cardUno = new Card($prodottoUno, $categoriaUno);
 echo $cardUno->printCard();
 
