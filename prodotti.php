420d65fc0438160b0ad6bca68a0598cc4636cc23
MonicaAuditore <mauditore@live.it>
2023-03-07 16:55:48 +0100

    add_exception in singoliProdotti - CardProdotti

--- prodotti.php
+++ prodotti.php
@@ -1,6 +1,11 @@
 <?php
 
+require_once __DIR__.'/traits/nome.php';
+
 class Prodotto {
+
+use nome;
+
   public $nome;
   public $descrizione;
   public $prezzo;
---
+++ traits/nome.php
@@ -0,0 +1,16 @@
+<?php
+
+trait nome {
+
+  protected $nome;
+
+  public function getNome() {
+    return $this->nome;
+  }
+
+  public function setNome($nome) {
+    $this->nome = $nome;
+  }
+}
+
+?>
